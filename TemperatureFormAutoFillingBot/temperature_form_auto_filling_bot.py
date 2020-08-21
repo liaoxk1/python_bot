ed4ce4e8ecd2e6763fcd2283c77966b2e6e9ac9f
liaoxk1 <37127734+liaoxk1@users.noreply.github.com>
2020-08-21 10:44:48 +0800

    Add files via upload
    Updated XPath for all buttons

--- TemperatureFormAutoFillingBot/temperature_form_auto_filling_bot.py
+++ TemperatureFormAutoFillingBot/temperature_form_auto_filling_bot.py
@@ -19,41 +19,35 @@ class Bot():
      
         btn_temp = self.driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[3]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/label/span/span')
         btn_temp.click()
-        sleep(2)
         
-        btn_next1 = self.driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[3]/div[3]/div/button')
+        btn_next1 = self.driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[3]/div[3]/div/div/button')
         btn_next1.click()
-        sleep(2)
+        sleep(1)
         
         btn_wfh = self.driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]/div[3]/div/div[10]/div/label/span/span')
         btn_wfh.click()
-        sleep(2)
         
         btn_yea = self.driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div[2]/div[3]/div/div[2]/div/label/span/span')
         btn_yea.click()
-        sleep(2)
        
-        btn_next2 = self.driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[3]/div[2]/button')
+        btn_next2 = self.driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[3]/div/div[2]/button')
         btn_next2.click()     
-        sleep(2)
+        sleep(1)
         
         pw_in = self.driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div[3]/div/div/div/input')
         pw_in.send_keys('40204886')
-        sleep(2)
         
-        btn_next3 = self.driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[3]/div[2]/button')
+        btn_next3 = self.driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[3]/div/div[2]/button')
         btn_next3.click()
-        sleep(2)
+        sleep(1)
         
         btn_good = self.driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div[1]/div[2]/div[3]/div/div[1]/div/label/span/span')
         btn_good.click()
-        sleep(2)
         
         btn_normal = self.driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div[2]/div[2]/div[3]/div/div[1]/div/label/span/span')
         btn_normal.click()
-        sleep(2)
         
-        btn_submit = self.driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[3]/div[2]/button')
+        btn_submit = self.driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div[1]/div/div[1]/div[2]/div[3]/div/div[2]/button')
         btn_submit.click()
         
 bot = Bot()
